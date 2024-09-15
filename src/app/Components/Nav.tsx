e664d893b5f85997c3ae3c6ee2702b8653c114b5
Salome-Bx <salome.burteaux.simplon@gmail.com>
2024-09-15 16:49:01 +0200

    responsive nav, footer, connexion

--- src/app/Components/Nav.tsx
+++ src/app/Components/Nav.tsx
@@ -1,36 +1,139 @@
-import React from 'react';
-import Button from './Button';
+// import React, { useState } from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 
-export const Nav = () => (
-  <div>
+
+
+export const Nav = () => {
+  const [showSubmenu, setShowSubmenu] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleMouseOver = () => {
+    setShowSubmenu(true);
+  };
+
+  const handleMouseLeave = () => {
+  
+    setTimeout(() => {
+      setShowSubmenu(false);
+    }, 500); 
+  };
+
+  return (
     <header className="w-full bg-custom-purple p-3 px-14">
-      <nav className="w-full flex flex-row">
 
-        <Link href="/" className="logo w-1/4 text-white"><img src="img/logo/logo.svg" alt="logo Adopte un Compagnon" /></Link>
+      
+      {/* --------Menu mobile avec modale-------- */}
+      <nav className="w-full flex flex-row lg:hidden xl:hidden 2xl:hidden justify-between">
 
-        <div className="links items-center justify-end flex flex-row w-3/4 text-white">
+          {/* Logo */}
+          <div className="logo">
+            <Link href="/" className="logo w-fit text-white">
+              <img src="img/logo/logo.svg" alt="logo Adopte un Compagnon" />
+            </Link>  
+          </div>
 
-          <Link href="/aPropos" className="flex mr-5 hover:underline">A propos</Link>
+          {/* Menu */}
+          <div className="menu flex flex-row">
+            <button className="connexion text-white">
+              <Link href="/connexion" className="items-center z-11 flex text-custom-light-purple color-white mr-4">
+                <Image
+                    src="/icones/user.png"
+                    width={21}
+                    height={24}
+                    alt="Espace mon compte"
+                />
+              </Link>
+            </button>
+            <button onClick={toggleMenu} className="items-center flex hover:text-custom-yellow text-white">
+              Menu
+            </button>
+            
+            {isOpen && (
+              <div className="menuMobile bg-custom-purple text-white p-4 rounded-lg shadow-md">
+                <ul className="flex flex-col space-y-4">
+                  <li>
+                    <Link href="/aPropos" className="flex mr-5 hover:text-custom-yellow">A propos</Link>
+                  </li>
+                  <li>
+                    <Link href="/animauxAdoption" className="flex mr-5 hover:text-custom-yellow">Compagnons à l'adoption</Link>
+                  </li>
+                  <li>
+                    <Link href="/sos" className="flex mr-5 hover:text-custom-yellow">SOS</Link>
+                  </li>
+                  <li>
+                    <Link href="/associations" className="flex mr-5 hover:text-custom-yellow">Les associations</Link>
+                  </li>
+                  <li>
+                    <Link href="/inscription" className=" text-custom-light-purple hover:underline">Inscription Association</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        
+      </nav>
 
-          <Link href="/animauxAdoption" className="flex mr-5 hover:text-custom-yellow">Compagnons à l'adoption</Link>
 
-          <Link href="/sos" className="flex mr-5 hover:text-custom-yellow">SOS</Link>
+      {/* --------Menu écrans larges-------- */}
+      <nav className="w-full hidden lg:block">
+        <div className="flex flex-row">
 
-          <Link href="/associations" className="flex mr-5 hover:text-custom-yellow">Les associations</Link>
-
-          <Link href="/inscription" className="border border-white text-white w-fit hover:text-custom-purple  hover:bg-white hover:borderwhite px-4 py-1 mr-5">Espace association</Link>
-
-          <Link href="/connexion"  className="bg-custom-light-purple border border-white text-white w-fit  px-4 py-1 mr-5 hover:text-custom-light-purple  hover:bg-white hover:borderwhite">Se connecter</Link>
-
+          {/* Logo */}
+          <div className="flex logo w-1/4 text-white">
+            <Link href="/">
+              <img src="img/logo/logo.svg" alt="logo Adopte un Compagnon"/>
+            </Link>  
+          </div>
+          {/* Menu */}
+          <div className="menuDesktop justify-end flex flex-row w-3/4 text-white">
+            <ul className="flex flex-row space-x-5 justify-end">
+              <li>
+                <Link href="/aPropos" className="text-white hover:text-custom-yellow">A propos</Link>
+              </li>
+              <li>
+                <Link href="/animauxAdoption" className="text-white hover:text-custom-yellow">Compagnons à l'adoption</Link>
+              </li>
+              <li>
+                <Link href="/sos" className="text-white hover:text-custom-yellow">SOS</Link>
+              </li>
+              <li>
+                <Link href="/associations" className="text-white hover:text-custom-yellow">Les associations</Link>
+              </li>
+              <li>
+                <Link 
+                  href=""
+                  onMouseEnter={handleMouseOver}
+                  onMouseLeave={handleMouseLeave}
+                  className="flex flex-col items-start text-custom-light-purple hover:underline relative z-10"
+                >
+                  Espace association
+                  {showSubmenu && (
+                    <ul className="absolute top-full left-0 mt-2 flex flex-col space-y-1 bg-custom-purple text-white p-2  shadow-md px-10">
+                      <li>
+                        <Link href="/connexion" className="text-sm hover:text-custom-light-purple">Connexion</Link>
+                      </li>
+                      <li>
+                        <Link href="/inscription" className="text-sm hover:text-custom-light-purple">Inscription</Link>
+                      </li>
+                    </ul>
+                  )}
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
 
       </nav>
+      
     </header>
-  </div>
+  );
+};
 
-
-)
 
